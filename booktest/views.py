ad22f637bfe356af68c5f47c97c94404eb9a5fd6
25036 <9723679jin>
2020-01-18 00:37:51 +0800

    定义REST接口

--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views import View
+'''
+RESTful风格：增加  post
+            删除 delete
+            修改  put
+            查询  get
+'''
 
-# Create your views here.
+class BookListView(View):
+    #获取所有的书籍
+    def get(self,request):
+        pass
+
+    #新增一本书籍
+    def post(self,request):
+        pass
+
+class BookView(View):
+
+
+    #获取指定的书籍
+    def get(self,request):
+        pass
+
+
+    #修改指定的书籍
+    def put(self,request):
+        pass
+
+    #删除指定的书籍
+    def delete(self,request):
+        pass
