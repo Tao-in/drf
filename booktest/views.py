9add391b05d0582e6edf2932c35e240c3b35837b
25036 <9723679jin>
2020-01-21 22:46:55 +0800

    APIView类的使用

--- booktest/views.py
+++ booktest/views.py
@@ -1,6 +1,10 @@
 import json
 from django.http import JsonResponse, HttpResponse
 from django.views import View
+from rest_framework.exceptions import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .serializers import *
 from .models import BookInfo
 '''
@@ -16,66 +20,78 @@ RESTful风格：增加  post
            
            通过序列化的data属性获取序列化的结果
            
+APIView是drf视图的基类，继承自django.View
+1,加强版的请求对象，获取请求对象更加方便
+2，加强版的响应对象，根据请求头中Accept字段自动包装响应数据
+3，封装了异常处理，常见http错误自动包装响应数据
+4，支持扩展功能，认证/权限/限流
+
+           
 '''
 
-class BookListView(View):
+class BookListView(APIView):
     #获取所有的书籍
     def get(self,request):
         books = BookInfo.objects.all()
         #
         serializer=BookInfoSerializer(books,many=True)
-        return JsonResponse(serializer.data,safe=False)#如果返回是列表需要设置safe参数为false
+        #drf的Response类型可以根据前端的需求（请求头的Accept字段）
+        return Response(serializer.data)
 
 
 
         # 新增一本书籍
     def post(self,request):
         #获取参数
-        book_dict=json.loads(request.body)
+        book_dict=request.data
+        # book_dict=json.loads(request.body)
         #创建序列化器，进行反序列化处理时设置data为请求参数的字典
         serializer=BookInfoSerializer(data=book_dict)
         # is_valid() 执行校验（数据的完整性， 数据的合法性，自定义的需求校验）
-        result=serializer.is_valid()
+        result=serializer.is_valid(raise_exception=True)
         '''
         校验的结果：result
         校验的错误信息：serializer.errors
         校验成功后的数据：serializer.validated_data
-        '''
-        if result==False:
-            return JsonResponse({'detail':serializer.errors},status=400)
+        # '''
+        # if result==False:
+        #     return JsonResponse({'detail':serializer.errors},status=400)
         #新增数据
         book=serializer.save()
         #返回结果
-        return JsonResponse(serializer.data,status=201)
+        return Response(serializer.data,status=201)
 
 
 
 
-class BookView(View):
+class BookView(APIView):
     #获取指定的书籍
     def get(self,request,pk):
         try:
             book=BookInfo.objects.get(pk=pk)
         except BookInfo.DoesNotExist:
-            return JsonResponse({'detail':'book not exist'},status=404)
+            raise NotFound()
+            # return JsonResponse({'detail':'book not exist'},status=404)
         serializer=BookInfoSerializer(book)
-        return JsonResponse(serializer.data,status=201)
+        return Response(serializer.data,status=201)
     #修改指定的书籍
     def put(self,request,pk):
         #获取书籍
-        book_dict=json.loads(request.body)
+        # book_dict=json.loads(request.body)
+        book_dict=request.data
 
         #查询书籍
         try:
             book=BookInfo.objects.get(pk=pk)
         except BookInfo.DoesNotExist:
-            return JsonResponse({'detail':'book not exist'},status=404)
+            raise NotFound()
+            # return JsonResponse({'detail':'book not exist'},status=404)
         serializer=BookInfoSerializer(book,data=book_dict)
-        result=serializer.is_valid()
-        if  result==False:
-            return JsonResponse({'errno':'参数错误'},status=400)
+        result=serializer.is_valid(raise_exception=True)
+        # if  result==False:
+        #     return JsonResponse({'errno':'参数错误'},status=400)
         book=serializer.save()
-        return JsonResponse(serializer.data,status=201)
+        return Response(serializer.data,status=201)
     #删除指定的书籍
     def delete(self,request,pk):
         #查询指定的书籍
--- drf_demo/settings.py
+++ drf_demo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booktest'
+    'booktest',
+    'rest_framework'#drf注册
 ]
 
 MIDDLEWARE = [
